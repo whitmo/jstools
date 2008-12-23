d202d560b3d83a2e873b2d1d3c66baece5a9e211
whit <whit@openplans.org>
2008-12-23 16:23:10 -0600

    add a test for circular deps (hint: we allow them)

--- tests/test_jsbuild.py
+++ tests/test_jsbuild.py
@@ -9,19 +9,21 @@ libdir, tempdir = None, None
 def setup(mod):
     mod.tempdir, mod.libdir = testutils.setup_temp_dir()
 
-def xtest_basic_config():
+def file_tree():
     merger = testutils.load_config('basic', libdir)
     handles = testutils.setup_dir(merger, prefix=libdir)
-    depmap = deps.DepMap.from_resource("data/deps1.cfg")
+    depmap = deps.DepMap.from_resource("data/deps-circular.cfg")
     files = [x for x in testutils.inject_deps(handles, libdir, depmap)]
     merger.set('Output.js', 'root', libdir)
     license_path = os.path.join(libdir, "license.txt")
     merger.set('Output.js', 'license', license_path)
-
     license_file = open(license_path, 'w')
     print >> license_file, license; license_file.close()
-    
-    out = merger.run(uncompressed=True, single='Output.js')
-    output = open(out[0])
-    import pdb;pdb.set_trace()
+    return merger
+
+def test_circular_deps_config():
+    merger = file_tree()
+    # eventually we may add some sort of "strict" checking
+    # but for now, the merg should run
+    assert merger.run(uncompressed=True, single='Output.js')
 
