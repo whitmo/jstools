5bf740da39b2caeeffee2e6435ebd8949fc5e551
whit <whit@openplans.org>
2008-12-23 12:12:11 -0600

    if paver is on the path already, skip minilib

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 import os
-if os.path.exists("paver-minilib.zip"):
-    import sys
-    sys.path.insert(0, "paver-minilib.zip")
+
+try:
+    import paver
+except ImportError:
+    if os.path.exists("paver-minilib.zip"):
+        import sys
+        sys.path.insert(0, "paver-minilib.zip")
 
 import paver.command
 paver.command.main()
