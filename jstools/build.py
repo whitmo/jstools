44f3ba44feeeebc7226cb36466bd209922093861
whit <whit@openplans.org>
2009-04-22 17:19:10 -0500

    * reworking of options
    * build is now an entry point (for easier extension probably a yagni)

--- jstools/build.py
+++ jstools/build.py
@@ -3,44 +3,56 @@ build.py
 
 Copyright (c) 2008 OpenGeo. All rights reserved.
 """
-
+import sys
 from jstools.merge import Merger
 from jstools.utils import arg_parser, printer as printer_factory
+import pkg_resources
 import optparse
 import os
 
 curdir = os.path.abspath(os.curdir)
+
 usage = "usage: %prog [options] filename1.cfg [filename2.cfg...]"
-parser = optparse.OptionParser(usage=usage)
-parser.add_option('-u', '--uncompress',
+default_parser = optparse.OptionParser(usage=usage)
+default_parser.add_option('-u', '--uncompress',
                   help="Don't compresses aggregated javascript",
                   action="store_true",
                   dest="uncompress",
                   default=False)
-parser.add_option('-v', '--verbose',
+default_parser.add_option('-v', '--verbose',
                   help="print more info",
                   action="store_true",
                   dest="verbose",
                   default=False)
-parser.add_option('-o', '--output',
+default_parser.add_option('-o', '--output',
                   help="Output directory",
                   action="store",
                   dest="output_dir",
                   default=curdir)
-parser.add_option('-r', '--resource',
+default_parser.add_option('-r', '--resource',
                   help="resource base directory (for interpolation)",
                   action="store",
                   dest="resource_dir",
                   default=curdir)
-parser.add_option('-s', '--single',
+default_parser.add_option('-j', '--just',
                   help="Only create file for this section",
                   action="store",
                   dest="single_file",
                   default=None)
-        
-#@@ test 
-@arg_parser(parser)
-def build(args=None, options=None, parser=None):
+default_parser.add_option('-s', '--single-file-build',
+                  help="Create a single file of all of possible output",
+                  action="store",
+                  dest="concat",
+                  default=None)
+default_parser.add_option('-c', '--compressor',
+                  help="Compressor plugin to use in form {specifier}:{'arguments_string'}",
+                  action="store",
+                  dest="compressor",
+                  default="default")
+
+
+@arg_parser(default_parser)
+def default_merge(args=None, options=None, parser=None):
     printer = printer_factory(options.verbose)
     if len(args) > 1:
         filenames = args[1:]
@@ -50,7 +62,13 @@ def build(args=None, options=None, parser=None):
                             output_dir=options.output_dir,
                             defaults={'resource-dir':options.resource_dir},
                             printer=printer)
-    out = merger.run(uncompressed=options.uncompress, single=options.single_file)
-    printer("Done: %s" %out, 0)
+    out = merger.run(uncompressed=options.uncompress, single=options.single_file, compressor=options.compressor)
+    printer("Done: %s" %out, 0)    
 
+
+def build():
+    dist = pkg_resources.get_distribution("jstools")
+    ep_map = pkg_resources.get_entry_map(dist, "jstools.jsbuild_command")
+    command = ep_map.get(sys.argv[1], "default")
+    command()
 
