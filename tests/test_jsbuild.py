5052f3bccd8a1be82094fefdee28a08e405c2a55
whit <whit@openplans.org>
2009-04-22 17:30:04 -0500

    add test for concatenation with and without a "meta" section

--- tests/test_jsbuild.py
+++ tests/test_jsbuild.py
@@ -5,17 +5,23 @@ import os
 
 libdir, tempdir = None, None
 
-def file_tree(conf='basic', depcfg='data/deps1.cfg'):
+license = "### LICENSE ###\n"
+
+def set_faux_files(cfg, libdir, *filenames):
+    for fn in filenames:
+        cfg.set(fn, 'root', libdir)
+        license_path = os.path.join(libdir, "license.txt")
+        cfg.set(fn, 'license', license_path)
+        open(license_path, 'w').write(license)
+
+def file_tree(conf='basic', depcfg='data/deps1.cfg', output_files=('Output.js',)):
+    #@@ add a dep->out map option
     tempdir, libdir = testutils.setup_temp_dir()
     merger = testutils.load_config(conf, libdir)
     handles = testutils.setup_dir(merger, prefix=libdir)
     depmap = deps.DepMap.from_resource(depcfg)
     files = [x for x in testutils.inject_deps(handles, libdir, depmap)]
-    merger.set('Output.js', 'root', libdir)
-    license_path = os.path.join(libdir, "license.txt")
-    merger.set('Output.js', 'license', license_path)
-    license_file = open(license_path, 'w')
-    print >> license_file, license; license_file.close()
+    set_faux_files(merger, libdir, *output_files)
     return merger
 
 def test_circular_deps_config():
@@ -45,3 +51,23 @@ def test_merger_by_file():
     filename = pkg_resources.resource_filename(DIST, "data/basic.cfg")
     assert merge.Merger.from_fn(filename, "/tmp")
     assert merge.Merger.from_fn([filename], "/tmp")
+
+import re
+R_FILES = re.compile(r"var filename='(.*)'")
+
+def test_concatenate():
+    """
+    Passing Merger.run a concatenate kwarg should create a single
+    """
+    merger = file_tree(depcfg="data/concat-dep.cfg", conf="meta-concatenate", output_files=("Output1.js", "Output2.js", "Output3.js"))
+    outfiles = merger.run(concatenate="sfb.js")
+    sfb = open(outfiles[0])
+    files_found = R_FILES.findall(sfb.read())
+    assert files_found == ['core2/lib2.js', 'core3/lib3.js', 'core1/lib1.js']
+
+    merger.add_section("meta")
+    merger.set("meta", "order", "\n".join(['Output1.js', 'Output2.js', 'Output3.js']))
+    outfiles = merger.run(concatenate="sfb2.js")
+    sfb = open(outfiles[0])
+    files_found = R_FILES.findall(sfb.read())
+    assert files_found == ['core1/lib1.js', 'core2/lib2.js', 'core3/lib3.js']
