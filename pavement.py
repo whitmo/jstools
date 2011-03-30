8aee287a2ccab49e935f8726c29f9b197c041ff9
Bruno Binet <bruno.binet@camptocamp.com>
2011-03-30 10:24:17 +0200

    bump version

--- pavement.py
+++ pavement.py
@@ -17,7 +17,7 @@ from ConfigParser import ConfigParser
 from paver import setuputils
 
 setuputils.install_distutils_tasks()
-version = '0.2.1'
+version = '0.2.2'
 
 try:
     description = ''.join([x for x in open('README.rst')])
